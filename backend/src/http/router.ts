c7a7f42d8cf58708339c7a4a6a90b62802ca002e
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-20 12:02:13 -0300

    addinng create to routes

--- backend/src/http/router.ts
+++ backend/src/http/router.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { testRoute } from "./controllers/test-route";
 import { findAllAnimalController } from "./controllers/find-all-animal-controller";
 import { findByIdAnimalController } from "./controllers/find-by-id-animal-controller";
+import { createAnimalController } from "./controllers/create-animal-controller";
 
 export async function appRouter(app: FastifyInstance) {
     app.get('/', testRoute);
@@ -9,5 +10,7 @@ export async function appRouter(app: FastifyInstance) {
     app.get('/animals', findAllAnimalController);
 
     app.get('/animals/:id', findByIdAnimalController);
+
+    app.post('/animals', createAnimalController);
     
 }
