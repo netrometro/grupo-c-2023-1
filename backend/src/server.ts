1c3f66d8d1b77ce0651fd9d1f5c5e7b3da59271b
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-19 23:43:16 -0300

    handle errors in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,23 @@
 import fastify from "fastify";
 import { appRouter } from "./http/router";
+import { ZodError } from "zod";
 
 const app = fastify()
 
 app.register(appRouter)
 
+app.setErrorHandler((error, _, res) => {
+    if (error instanceof ZodError) {
+      return res
+        .status(422)
+        .send({ message: 'Validation error.', issues: error.format() })
+    }
+
+    console.log(error)
+
+    return res.status(500).send({ message: 'Internal server error.' })
+});
+
 app.listen({
     port: 8080
-}).then(server => { console.log(`HTTP server running at ${server}`) })
+}).then(server => { console.log(`HTTP server running at ${server}`) });
