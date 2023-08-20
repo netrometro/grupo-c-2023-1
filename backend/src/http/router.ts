62151a26127bc07cfa14b76787b4e34b0dc326c2
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-19 23:41:51 -0300

    add finds to routes

--- backend/src/http/router.ts
+++ backend/src/http/router.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { testRoute } from "./controllers/test-route";
+import { findAllAnimalController } from "./controllers/find-all-animal-controller";
+import { findByIdAnimalController } from "./controllers/find-by-id-animal-controller";
 
 export async function appRouter(app: FastifyInstance) {
-    app.get('/', testRoute)
+    app.get('/', testRoute);
+
+    app.get('/animals', findAllAnimalController);
+
+    app.get('/animals/:id', findByIdAnimalController);
+    
 }
