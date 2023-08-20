d0ac3229d66f6e74985963d05269f87adfc259d7
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-20 11:51:49 -0300

    adding create and findBySpecie to IAnimalsrepository interface

--- backend/src/repository/i-animals-repository.ts
+++ backend/src/repository/i-animals-repository.ts
@@ -1,6 +1,8 @@
 import { Animal, Prisma } from "@prisma/client";
 
 export interface IAnimalsRepository {
+    create: (animalDto: Prisma.AnimalCreateInput) => Promise<Animal>
     findAll: () => Promise<Animal[]>
     findById: (animalId: number) => Promise<Animal | null>
+    findBySpecie: (specie: string) => Promise<Animal | null>
 }
