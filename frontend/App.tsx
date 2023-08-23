201695bfd045dd2ff2313c9b673dfec79b7f4655
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-22 21:18:11 -0300

    putting react navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+// import { ListAnimals } from "./src/screens/listAnimals";
+// import CreateAnimal from "./src/components/createAnimal";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator>
+          {/* <Stack.Screen name="ListAnimals" component={ListAnimals} />
+          <Stack.Screen name="CreateAnimal" component={CreateAnimal} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
