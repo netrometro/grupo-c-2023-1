853c31aaafe4cfdd71aae559d43cb73a1050af06
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-27 00:18:43 -0300

    creating conservation_status.ts

---
+++ frontend/src/conservation_status.ts
@@ -0,0 +1,9 @@
+type ConservationStatus = "EXTINCT" |
+    "EXTINCT_IN_THE_WILD" |
+    "CRITICAL_ENDANGERED" |
+    "ENDANGERED" |
+    "VULNERABLE" |
+    "NEAR_THREATENED" |
+    "LEAST_CONCERN" |
+    "DATA_DEFICIENT" |
+    "NOT_AVALUATED"
