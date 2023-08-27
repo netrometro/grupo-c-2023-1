c6fc97fd144544424a683a0e3b48a018f44bd880
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-27 00:25:41 -0300

    adjustments in project

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,19 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-// import { ListAnimals } from "./src/screens/listAnimals";
-// import CreateAnimal from "./src/components/createAnimal";
-
-const Stack = createNativeStackNavigator();
+import { Routes } from "./src/routes";
 
 export default function App() {
   return (
     <NavigationContainer>
-        <StatusBar style="auto" />
-        <Stack.Navigator>
-          {/* <Stack.Screen name="ListAnimals" component={ListAnimals} />
-          <Stack.Screen name="CreateAnimal" component={CreateAnimal} /> */}
-        </Stack.Navigator>
+        <StatusBar style="auto" hidden />
+        <Routes />
       </NavigationContainer>
   );
 }
