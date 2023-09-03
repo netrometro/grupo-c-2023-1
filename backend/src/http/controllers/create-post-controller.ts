5bf4366ae5cc852c3a4824fa3d28dce6679391df
Amadeu Santos <amadeusantos31@gmail.com>
2023-09-03 11:50:46 -0300

    creating create-post-controller

---
+++ backend/src/http/controllers/create-post-controller.ts
@@ -0,0 +1,47 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { PrismaPostsRepository } from "../../repository/prisma/prisma-posts-repository";
+import { CreatePostUseCase } from "../../use-cases/create-post/create-post-use-case";
+
+export async function createPostController(request:FastifyRequest, response: FastifyReply) {
+    const createPostValidationSchema = z.object({
+        title: z.string(),
+        description: z.string(),
+        image: z.string()
+    });
+
+    const { description, image, title } = createPostValidationSchema.parse(request.body);
+
+    try {
+        const prismaPostsRepository = new PrismaPostsRepository();
+        const createPostUseCase = new CreatePostUseCase(prismaPostsRepository);
+
+        const post = createPostUseCase.handle({ description, image, title});
+
+        const postSchema = z.object({
+            id: z.number(),
+            title: z.string(),
+            description: z.string().transform(description => description.substring(0, 50)),
+            url_image: z.string() || null,
+            published_at: z.date() || null,
+            user: z.object({
+                username: z.string(),
+            }).transform(user => user.username),
+            _count: z.object({
+                likes: z.number(),
+            }).transform(count => count.likes),
+        });
+
+        const { _count: likes, ...rest } = postSchema.parse(post);
+
+        return response.send({
+            post: {
+                likes,
+                ...rest
+            }
+        });
+    } catch (error) {
+        
+    }
+    
+}
