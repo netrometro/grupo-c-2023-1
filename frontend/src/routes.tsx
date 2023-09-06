6e800050d056f3db81c255112c25133ef4871fb6
Amadeu Santos <amadeusantos31@gmail.com>
2023-09-06 02:57:31 -0300

    fix routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,19 +2,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ListAnimals } from "./screens/listAnimals";
 import CreateAnimal from "./screens/createAnimal";
 import { AnimalInfor } from "./screens/animalInfor";
+import { ListPosts } from "./screens/listPosts";
+import { PostInfor } from "./screens/postInfor";
 
 
 type RootParam = {
     CreateAnimal: undefined;
     Animais: undefined;
-    AnimalInfor: { id: number }
+    AnimalInfor: { id: number };
+    ListPosts: undefined;
+    PostInfor: { id: number };
 }
 
 
 export function Routes() {
     const {Group, Navigator, Screen} = createNativeStackNavigator<RootParam>();
+
+    // teste
+
   return (
-    <Navigator initialRouteName="Animais">
+    <Navigator initialRouteName="ListPosts">
+      <Screen name="ListPosts" component={ListPosts} />
+      <Screen name="PostInfor" component={PostInfor} /> 
       <Screen name="Animais" component={ListAnimals} />
       <Screen name="CreateAnimal" component={CreateAnimal} />
       <Screen name="AnimalInfor" component={AnimalInfor} /> 
