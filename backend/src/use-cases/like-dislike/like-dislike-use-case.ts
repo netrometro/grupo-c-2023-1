0b17be9496f79ac9d5f3ff964eec5c9de36c4c95
Rafael Souza <rafaeljosegomes25@gmail.com>
2023-09-05 22:17:51 -0300

    created like-dislike use case

---
+++ backend/src/use-cases/like-dislike/like-dislike-use-case.ts
@@ -0,0 +1,49 @@
+import { ILikesRepository } from "../../repository/i-likes-repository";
+import { IPostsRepository } from "../../repository/i-posts-repository";
+import { IUsersRepository } from "../../repository/i-users-repository";
+import { PostNotFoundError, UserNotFoundError } from "../global-errors";
+import { LikeDislikeUseCaseRequest, LikeDislikeUseCaseResponse } from "./dtos";
+
+export class LikeDislikeUseCase {
+    constructor(
+        private usersRepository: IUsersRepository,
+        private postsRepository: IPostsRepository,
+        private likesRepository: ILikesRepository
+    ) {}
+
+    async handle({
+        postId,
+        userId
+    }: LikeDislikeUseCaseRequest): Promise<LikeDislikeUseCaseResponse> {
+        const post = await this.postsRepository.findById(postId)
+
+        if (!post) {
+            throw new PostNotFoundError()
+        }
+
+        const isLiked = !!(await this.likesRepository.find(userId, postId))
+
+        if (isLiked) {
+            return { isLiked }
+        }
+
+        await this.likesRepository.create({
+            post_id: postId,
+            user_id: userId
+        })
+
+        const user = await this.usersRepository.findById(userId)
+
+        if (!user) {
+            throw new UserNotFoundError()
+        }
+
+        const userFishCoins = user.point ?? 0
+
+        await this.usersRepository.update(userId, {
+            point: userFishCoins + 5
+        })
+
+        return { isLiked }
+    }
+}
