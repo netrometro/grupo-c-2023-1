523129347cd4445af74fb06118b221c986e390c3
Amadeu Santos <amadeusantos31@gmail.com>
2023-09-09 02:18:20 -0300

    creating styles de Login

---
+++ frontend/src/screens/login/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#0984E3"
+    },
+    button: {
+        backgroundColor: "#fff",
+        padding: 12, borderRadius: 16
+    },
+    textButton: { fontWeight: "bold" }
+})
