339f2e1f58d9fb89a7813b53343ded01a7d3a42d
Rafael Souza <rafaeljosegomes25@gmail.com>
2023-09-13 00:00:24 -0300

    implemented unit tests to find user by id

--- backend/src/repository/in-memory/users-in-memory-repository.ts
+++ backend/src/repository/in-memory/users-in-memory-repository.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client";
+import { $Enums, Prisma } from "@prisma/client";
 import { InMemoryData } from "../../@types/in-memory-data";
 import { IUsersRepository } from "../i-users-repository";
 import { IInMemoryRepository } from "../i-in-memory-repository";
@@ -7,6 +7,23 @@ export class UsersInMemoryRepository implements IUsersRepository, IInMemoryRepos
     constructor(
         readonly data: InMemoryData
     ) {}
+
+    async findByGithubId(github_id: string) {
+        return this.data.users.find(user => user.github_id === github_id) || null
+    }
+
+    async create(user: Prisma.UserCreateInput) {
+        this.data.users.push({
+            id: this.data.users.length + 1,
+            avatar_url: "",
+            github_id: "",
+            point: 0,
+            role: user.role,
+            username: user.username
+        })
+
+        return this.data.users[this.data.users.length - 1]
+    }
         
     async findById(userId: number) {
         return this.data.users.find(user => user.id === userId) || null
@@ -18,8 +35,7 @@ export class UsersInMemoryRepository implements IUsersRepository, IInMemoryRepos
         const validDataPoints = Number(data.point) >= 0
 
         user.avatar_url = data.avatar_url ? String(data.avatar_url) : user.avatar_url
-        user.email = data.email ? String(data.email) : user.email
-        user.google_id = data.google_id ? String(data.google_id) : user.google_id
+        user.github_id = data.github_id ? String(data.github_id) : user.github_id
         user.point = validDataPoints ? Number(data.point) : user.point
         user.username = data.username ? String(data.username) : user.username
 
